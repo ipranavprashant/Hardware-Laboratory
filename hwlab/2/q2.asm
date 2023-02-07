0800e4f06753b62dfc9470d3b38f60cb63b674e8
Pranav Prashant <97225953+ipranavprashant@users.noreply.github.com>
2023-02-07 23:52:56 +0530

    Update q2.asm

--- hwlab/2/q2.asm
+++ hwlab/2/q2.asm
@@ -1,3 +1,8 @@
+;Made by Pranav Prashant
+;Inorder to run this program:
+;Compile: nasm -f elf q4.asm
+;Link: ld -melf_i386 q3.o -o q3
+
 section .data
 msg1:db 'Enter the number',0Ah
 l1:equ $-msg1
@@ -116,3 +121,7 @@ popa
 ret
 
 
+;Made by Pranav Prashant
+;Inorder to run this program:
+;Compile: nasm -f elf q4.asm
+;Link: ld -melf_i386 q3.o -o q3
